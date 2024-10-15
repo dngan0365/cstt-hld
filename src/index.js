b4689b2b466af7b0d67ded320b739aa3c2cacde5
Dang Ngan <dngan0365@gmail.com>
2024-10-15 19:51:10 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Router>
         <GlobalStyles>
             <App />
         </GlobalStyles>
-    </React.StrictMode>,
+    </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
